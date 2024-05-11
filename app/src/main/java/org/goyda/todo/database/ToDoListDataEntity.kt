5633fb1de02e1e379468c0d57a85b2a79d19cdc0
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-05-12 00:21:14 +0500

    Рефакторинг

--- app/src/main/java/org/goyda/todo/database/ToDoListDataEntity.kt
+++ app/src/main/java/org/goyda/todo/database/ToDoListDataEntity.kt
@@ -6,11 +6,22 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "todolist")
 data class ToDoListDataEntity (
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
-    @ColumnInfo(name = "title" ) val title : String ="",
-    @ColumnInfo(name = "date" ) val date : String="",
-    @ColumnInfo(name = "desc" ) val desc : String ="",
-    @ColumnInfo(name = "time" ) val time : String="",
-    @ColumnInfo(name = "isShow" ) val isShow : Int=0//,
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+
+    @ColumnInfo(name = "title" )
+    val title : String ="",
+
+    @ColumnInfo(name = "date" )
+    val date : String="",
+
+    @ColumnInfo(name = "desc" )
+    val desc : String ="",
+
+    @ColumnInfo(name = "time" )
+    val time : String="",
+
+    @ColumnInfo(name = "isShow" )
+    val isShow : Int=0//,
     //@ColumnInfo(name= "comp") val comp: Boolean=false
 )
