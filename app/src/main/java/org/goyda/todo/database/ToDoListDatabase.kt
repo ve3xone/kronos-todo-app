542b5b96a7d912a04ec066fd1f8918c75c745c2c
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-06-10 02:15:45 +0500

    Добавлены алгоритмы шифрования и обработки
    Вшит в приложение ключ на base64;
    base64 переводится в md5 и все это становится общим паролем для бд;
    Также переделана инциализация бд;

--- app/src/main/java/org/goyda/todo/database/ToDoListDatabase.kt
+++ app/src/main/java/org/goyda/todo/database/ToDoListDatabase.kt
@@ -1,9 +1,13 @@
 package org.goyda.todo.database
 
 import android.content.Context
+import android.util.Base64
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import java.security.MessageDigest
 
 @Database(entities = arrayOf(ToDoListDataEntity::class), version = 1)
 abstract class ToDoListDatabase : RoomDatabase()
@@ -14,16 +18,35 @@ abstract class ToDoListDatabase : RoomDatabase()
         @Volatile
         private var instanse : ToDoListDatabase? = null
 
-        fun getInstance(context: Context): ToDoListDatabase? {
-            if (instanse == null) {
+        // Гавнокод)) : Моя кодовая фраза
+        //BASE64 xD
+        private const val passwordApp: String = "0JzQtNCwINGD0LYg0LrQsNC6INC20LUg0Y8g0LfQsNC00L7Qu9Cx0LDQu9GB0Y8g0LTQtdC70LDRgtGMINC/0YDQvtC10LrRgiDQtNC70Y8g0L/RgNC+0LXQutGC0L3QvtCz0L4g0L/RgNCw0LrRgtC40LrRg9C80LAg0LrQvtGC0L7RgNGL0Lkg0LzQvdC1INC90LjQutC+0LPQtNCwINC90LUg0L/RgNC40LPQvtC00LjRgtGB0Y8uLi4="
+
+        fun getInstance(context: Context, passwordUser: String): ToDoListDatabase? {
+            //if (instanse == null) {
                 synchronized(ToDoListDatabase::class) {
+                    val passphrase: ByteArray = SQLiteDatabase.getBytes(base64ToMD5(passwordApp).toCharArray()+
+                                                                              base64ToMD5(stringToBase64(passwordUser)).toCharArray())
+                    val factory = SupportFactory(passphrase)
                     instanse = Room.databaseBuilder(context.applicationContext,
                         ToDoListDatabase::class.java, "todo-db")
+                        .openHelperFactory(factory)
                         .allowMainThreadQueries()
                         .build()
                 }
-            }
+            //}
             return instanse
+        }
+
+        private fun stringToBase64(input: String): String {
+            return Base64.encodeToString(input.toByteArray(), Base64.DEFAULT).trim()
+        }
+
+        private fun base64ToMD5(base64String: String): String {
+            val bytes = base64String.toByteArray()
+            val md = MessageDigest.getInstance("MD5")
+            val digest = md.digest(bytes)
+            return digest.joinToString("") { "%02x".format(it) }
         }
     }
 }
