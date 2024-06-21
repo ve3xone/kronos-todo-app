550d9745a852cc93a43d0ad527f983ab6fd74094
Vladislav Startsev <39828362+ve3xone@users.noreply.github.com>
2024-06-21 17:48:22 +0500

    Проверки перед уведомлением

--- app/src/main/java/org/goyda/todo/notification/AlarmReceiver.kt
+++ app/src/main/java/org/goyda/todo/notification/AlarmReceiver.kt
@@ -13,6 +13,9 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import org.goyda.todo.R
 import org.goyda.todo.database.ToDoListDatabase
+import java.util.Calendar
+import java.util.Locale
+import java.util.TimeZone
 
 class AlarmReceiver : BroadcastReceiver() {
     private val GROUP_MESSAGE: String = "TODOLIST"
@@ -46,6 +49,9 @@ class AlarmReceiver : BroadcastReceiver() {
         val completeIntent = Intent(context, CompleteActionReceiver::class.java).apply {
             putExtra("id", dbId)
             putExtra("pass", pass)
+            putExtra("title", title)
+            putExtra("desc", desc)
+            putExtra("date", time)
         }
         val completePendingIntent = PendingIntent.getBroadcast(context, dbId.toInt(), completeIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
@@ -61,12 +67,11 @@ class AlarmReceiver : BroadcastReceiver() {
             .addAction(0, context.getString(R.string.done), completePendingIntent)
             .build()
 
-        if (toDoListDatabase?.toDoListDao()?.get(dbId) != null) {
-            if (!toDoListDatabase?.toDoListDao()?.get(dbId)!!.comp) {
+        toDoListDatabase?.toDoListDao()?.get(dbId)?.let { task ->
+            if (!task.comp && task.title == title && task.desc == desc && "${task.date} ${task.time}" == time) {
                 notificationManager.notify(dbId.toInt(), notification)
-                toDoListDatabase?.toDoListDao()?.isShownUpdate(id = dbId, isShow = 1)
-                val list = toDoListDatabase?.toDoListDao()?.get(dbId)
-                Log.d("IsRead", "isRead " + list?.isShow)
+                toDoListDatabase!!.toDoListDao().isShownUpdate(id = dbId, isShow = 1)
+                Log.d("IsRead", "isRead ${task.isShow}")
             }
         }
     }
@@ -80,9 +85,17 @@ class AlarmReceiver : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             val dbId = intent.getLongExtra("id", -1)
             val pass = intent.getStringExtra("pass") ?: ""
+            val title = intent.getStringExtra("title") ?: ""
+            val desc = intent.getStringExtra("desc") ?: ""
+            val time = intent.getStringExtra("date") ?: ""
+
             val toDoListDatabase = ToDoListDatabase.getInstance(context, pass)
-            toDoListDatabase?.toDoListDao()?.compUpdate(dbId, true)
-            Log.d("CompleteActionReceiver", "Task $dbId marked as complete")
+            toDoListDatabase?.toDoListDao()?.get(dbId)?.let { task ->
+                if (!task.comp && task.title == title && task.desc == desc && "${task.date} ${task.time}" == time) {
+                    toDoListDatabase.toDoListDao().compUpdate(dbId, true)
+                    Log.d("CompleteActionReceiver", "Task $dbId marked as complete")
+                }
+            }
 
             val notificationManager = context.getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
             notificationManager.cancel(dbId.toInt())
